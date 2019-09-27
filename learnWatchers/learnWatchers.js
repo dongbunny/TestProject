f6e33e57ac00d69adc20a7e0279aa336ffa7cf28
dongbunny <30820609+dongbunny@users.noreply.github.com>
2019-09-27 16:14:55 +0900

    Update learnWatchers.js

--- learnWatchers/learnWatchers.js
+++ learnWatchers/learnWatchers.js
@@ -1,3 +1,4 @@
+<script src="https://unpkg.com/vue"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
 var watchExampleVM = new Vue({
